a23b5d5bcd53c62f5d01bf18f502bdbc38bc6a66
piaand <pia.m.andersin@gmail.com>
2020-07-08 16:06:35 +0300

    added more basis to Rule class

--- src/main/java/com/piaandersin/pagecheck/Rule.java
+++ src/main/java/com/piaandersin/pagecheck/Rule.java
@@ -1,16 +1,26 @@
-
 package com.piaandersin.pagecheck;
-import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * This is a template for the Class of Rules which give the base for checking
  * content against its requirements
- * 
+ *
  * An example of a rule could be that a given character array must appear in the
- * rule.
+ * rule. Therefore the category of a rule could be called "exists" and body od a
+ * rule would have the word to be matched.
  */
-
-@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rule {
-    
+
+    private String body;
+    private String category;
+
+    public void mapRules() {
+        //decide which rules to check according to category.
+    }
 }
